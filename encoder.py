b5289bed007f482d6b25ff870971262e2c8944fd
boone-nguyen <148978686+boone-nguyen@users.noreply.github.com>
2023-11-01 13:58:57 -0400

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,36 @@
+def encoder(password):
+    val = []
+    encoded = ""
+    for i in password:
+        val.append(i)
+    for j in range(len(val)):
+        val[j] = int(val[j]) + 3
+    for e in range(len(val)):
+        val[e] = str(val[e])
+    for g in range(len(val)):
+        val[g] = val[g][-1]
+    for h in range(len(val)):
+        encoded += val[h]
+    return encoded
+
+
+def print_menu():
+    print("menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+
+
+if __name__ == '__main__':
+    continue_run = True
+
+    while continue_run:
+        print_menu()
+        option = input("Please enter an option: ")
+        if option == "1":
+            password = encoder(input("Please enter your password to encode:"))
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            pass
+        elif option == "3":
+            continue_run = False
+            break
+
+    print("Thank you for using password encoder")
